aaf40d92717156127fd3356021ba7a9260d9e0ab
pi <2770825992@qq.com>
2026-03-15 16:59:39 +0800

    四、AI应用生成 实现应用生成以及应用部署

---
+++ src/main/java/com/ppp/piaicodemother/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.ppp.piaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.ppp.piaicodemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/pjiayu">程序员皮皮皮</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/ppp/piaicodemother/service/AppService.java
@@ -0,0 +1,65 @@
+package com.ppp.piaicodemother.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.ppp.piaicodemother.model.dto.app.AppQueryRequest;
+import com.ppp.piaicodemother.model.entity.App;
+import com.ppp.piaicodemother.model.entity.User;
+import com.ppp.piaicodemother.model.vo.AppVO;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/pjiayu">程序员皮皮皮</a>
+ */
+
+public interface AppService extends IService<App> {
+
+    /**
+     * 通过对话生成应用代码
+     *
+     * @param appId 应用 ID
+     * @param message 提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    /**
+     * 应用部署
+     *
+     * @param appId 应用 ID
+     * @param loginUser 登录用户
+     * @return 可访问的部署地址
+     */
+    String deployApp(Long appId, User loginUser);
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+}
+
